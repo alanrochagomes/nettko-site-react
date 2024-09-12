ef6df338e2c748cb5b3d5af67543ff9b53ee4635
alanrochagomes <alanrochaarg2001@gmail.com>
2024-09-12 18:06:06 -0300

    update videos again

--- src/app/components/pages/gameplays-videos/service/youtubeService.js
+++ src/app/components/pages/gameplays-videos/service/youtubeService.js
@@ -1,25 +1,19 @@
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 const CHANNEL_ID = process.env.REACT_APP_YOUTUBE_CHANNEL_ID;
-const MAX_RESULTS = 50;
+const MAX_RESULTS = 10;
 
-export const fetchVideos = async (pageToken = "") => {
+export const fetchVideos = async () => {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${MAX_RESULTS}&pageToken=${pageToken}`
+      `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${MAX_RESULTS}`
     );
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
-    return {
-      fetchedVideos: data.items,
-      newNextPageToken: data.nextPageToken || null,
-    };
+    return data.items;
   } catch (error) {
     console.error("Failed to fetch videos:", error);
-    return {
-      fetchedVideos: [],
-      newNextPageToken: null,
-    };
+    return [];
   }
 };
