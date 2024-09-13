3d2668f2c5cdf509ab64aef3c8742fc3d6ba9f84
alanrochagomes <alanrochaarg2001@gmail.com>
2024-09-13 17:37:07 -0300

    add: funcionalidade na `playlist`

--- src/app/components/routes/app.component.routes.js
+++ src/app/components/routes/app.component.routes.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomeComponent from "../pages/home/home.component";
 import VideosGames from "../pages/videos/videos-games/videos.component";
 import VideoPlayerGames from "../pages/videos/videos-games/VideoPlayerComponent";
-import PlaylistsGames from "../pages/playlists/playlists-games/playlist.component";
+import PlaylistsGames from "../pages/playlists/playlists-games/ListVideosPlayer";
+import VideoDetailsPage from "../pages/playlists/playlists-games/VideoDetailsPage";
 import LivesComponent from "../pages/lives/live.component";
 import NewsComponent from "../pages/news-games/news.component";
 // import PodcastTi from "../pages/podcast/podcast-ti/PodcastTi";
@@ -18,23 +19,18 @@ const App = () => {
   return (
     <Router>
       <NavbarComponent />
-
       <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/videos/gameplays" element={<VideosGames />} />
-        <Route path="/playlists/games" element={<PlaylistsGames />} />
+        <Route path="/playlists/gameplays" element={<PlaylistsGames />} />
         <Route path="/lives" element={<LivesComponent />} />
         <Route path="/news/games" element={<NewsComponent />} />
-        {/* <Route path="/podcast/games" element={<PodcastGames />} /> */}
-        {/* <Route path="/podcast/ti" element={<PodcastTi />} /> */}
         <Route path="/member" element={<MemberComponent />} />
         <Route path="/about" element={<AboutComponent />} />
         <Route path="/help" element={<HelpComponent />} />
-
-        {/* Adicionar rota para o player interno */}
         <Route path="/watch/:videoId" element={<VideoPlayerGames />} />
+        <Route path="/video/:videoId" element={<VideoDetailsPage />} />
       </Routes>
-
       <FooterComponent />
     </Router>
   );
